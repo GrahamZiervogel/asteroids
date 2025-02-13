f5b0839b51e782e41c47db71ffbbae2958d411d3
GrahamZiervogel <grahamziervogel@gmail.com>
2025-02-13 16:04:33 +0000

    Create and draw player object

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     clock = pygame.time.Clock()
     dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +21,7 @@ def main():
                 return
             
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
